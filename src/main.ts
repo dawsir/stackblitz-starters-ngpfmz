b5ee6e40def4426c742d9da880ce0f4cabf69840
dawsir <git@stackblitz.com>
2024-10-08 14:03:42 +0200

    Create detail page, further implementation

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,32 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { DropdownComponent } from './dropdown/dropdown.component';
 import {PokemonService, PokemonResponse, PokemonBasic} from './pokemon.service'
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    DropdownComponent,
   ],
   template: `
-  <div class="title">
+  <div class="wrapper">
   <h1>PokeSelect</h1>
-</div> 
-<div class="dropdown">
-</div>
-<div class="details">
-</div> 
-    <!-- <app-dropdown></app-dropdown> -->
+    <app-dropdown></app-dropdown>
+
+  </div>
+
   `,
 })
 export class App implements OnInit {
   service: PokemonService = inject(PokemonService);
-  pokemons: BehaviorSubject<PokemonBasic[]> = new BehaviorSubject([]);
+  pokemons: BehaviorSubject<PokemonBasic[]> = new BehaviorSubject<PokemonBasic[]>([]);
 
   ngOnInit(): void {
     this.service.getPokemons().subscribe((value: PokemonResponse) => {
+      this.pokemons.next(value.results);
       console.log(value);
     })
   }
---
+++ src/pokemon-details/pokemon-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-details',
+  standalone: true,
+  imports: [],
+  template: '<p>pokemon-details works!</p>',
+  styleUrl: './pokemon-details.component.css'
+})
+export class PokemonDetailsComponent {
+
+}
